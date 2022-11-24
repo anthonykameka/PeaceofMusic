c9addc7bf05ae7da2e56e277c43090c4813fba16
“Anthony <antkameka@gmail.com>
2022-11-24 14:19:23 -0500

    trying to install dependencies

--- server/index.js
+++ server/index.js
@@ -1,11 +1,41 @@
 const express = require('express')
 const helmet = require("helmet");
 const morgan = require("morgan")
+const { auth } = require('express-openid-connect');
 const port = 8000
 
-express()
 
-    .use(express.json())
+express()
+.use(function (req, res, next) {
+    res.header(
+        "Access-Control-Allow-Methods",
+        "OPTIONS, HEAD, GET, PUT, POST, DELETE"
+        );
+        res.header(
+            "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+        );
+        next();
+    })
+
+        .use(express.json())
+        .use(morgan("tiny"))
+        .use(express.urlencoded({ extended: false }))
+        .use("/", express.static(__dirname + "/"))
+        .use(function (req, res, next) {
+        res.header(
+        "Access-Control-Allow-Methods",
+        "OPTIONS, HEAD, GET, PUT, POST, DELETE"
+        );
+        res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+        );
+        next();
+    })
+
+
+    /// ENDPOINTS
 
     .get('/hello', (req, res) => {
     res.status(200).json({status: 200, message: "Hello World!" })
